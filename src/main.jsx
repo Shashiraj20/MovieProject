b0e5bfa1086f6fdb9a1a344c9e8043308b0eaf47
Shashi Raj <shashiraj20@gmail.com>
2026-03-03 12:07:10 +0530

    add index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./css/index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+git add .
+git commit -m "add index.css"
+git push
